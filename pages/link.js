0d0fa7d54e41d6f4b947723b58c15f10ae67a1c5
Juke-99 <yanpahika@gmail.com>
2019-04-20 21:33:57 +0900

    first commit

---
+++ pages/link.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Linker = () => (
+	<div>
+		<Link href="/about">
+			<a>About Page</a>
+		</Link>
+
+		<p>Hello there!!</p>
+	</div>
+)
+
+export default Linker
